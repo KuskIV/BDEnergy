6b3c0deea2760fb86b32f10ca1489ae71a690348
Mads Kusk <madshkusk@gmail.com>
2023-03-14 13:17:55 +0100

    rapl implementation

--- bd-energy-framework/BDEnergyFramework/MeasuringInstruments/Rapl.cs
+++ bd-energy-framework/BDEnergyFramework/MeasuringInstruments/Rapl.cs
@@ -1,3 +1,7 @@
+using BDEnergyFramework.Models;
+using BDEnergyFramework.Models.Internal;
+using System.Timers;
+
 namespace BDEnergyFramework.MeasuringInstruments;
 
 public class Rapl : MeasuringInstrument
@@ -5,5 +9,24 @@ public class Rapl : MeasuringInstrument
     public Rapl(EMeasuringInstrument measuringInstrument) : base(measuringInstrument)
     {
     }
-    
+
+    internal override (TimeSeries, Measurement) ParseData(string path, DateTime startTime, DateTime endTime, long elapsedMilliseconds, double startTemperature, double endTemperature, int iteration)
+    {
+        return base.ParseData(path, startTime, endTime, elapsedMilliseconds, startTemperature, endTemperature, iteration);
+    }
+
+    internal override void PerformMeasuring(object sender, ElapsedEventArgs e)
+    {
+        base.PerformMeasuring(sender, e);
+    }
+
+    internal override void StartMeasuringInstruments(string path)
+    {
+        base.StartMeasuringInstruments(path);
+    }
+
+    internal override void StopMeasuringInstrument()
+    {
+        base.StopMeasuringInstrument();
+    }
 }
