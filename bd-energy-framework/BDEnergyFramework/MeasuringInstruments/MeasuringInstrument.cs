e89411287d2e52f1ba575df4f422db337e3fdddb
Mads Kusk <madshkusk@gmail.com>
2023-03-17 09:49:00 +0100

    parse file path fix

--- bd-energy-framework/BDEnergyFramework/MeasuringInstruments/MeasuringInstrument.cs
+++ bd-energy-framework/BDEnergyFramework/MeasuringInstruments/MeasuringInstrument.cs
@@ -28,14 +28,14 @@ namespace BDEnergyFramework.MeasuringInstruments
             return _milisecondsBetweenSampels;
         }
 
-        public (TimeSeries, Measurement) GetMeasurement(DateTime startTime, DateTime endTime, long elapsedMilliseconds, double startTemperature, double endTemperature, int iteration, bool burninApplied)
+        public (TimeSeries, Measurement) GetMeasurement(DateTime startTime, DateTime endTime, long elapsedMilliseconds, double startTemperature, double endTemperature, int iteration, bool burninApplied, DateTime fileCreatingTime)
         {
             if (!burninApplied)
             {
                 return (new TimeSeries(), new Measurement());
             }
 
-            var path = PathUtils.GetResultPathForMeasuringInstrument(_measuringInstrument, startTime);
+            var path = PathUtils.GetResultPathForMeasuringInstrument(_measuringInstrument, fileCreatingTime);
 
             return ParseData(path, startTime, endTime, elapsedMilliseconds, startTemperature, endTemperature, iteration);
         }
